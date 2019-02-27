7393fcbb44f4c1b566b4f8ea96d543169e1cb9ba
ssl <17610305051@163.co>
2019-02-27 14:59:03 +0800

    ???

--- app/src/main/java/com/example/ssl/t_mvvmtest/base/BaseRepository.kt
+++ app/src/main/java/com/example/ssl/t_mvvmtest/base/BaseRepository.kt
@@ -2,18 +2,11 @@ package com.example.ssl.t_mvvmtest.base
 
 import com.example.ssl.t_mvvmtest.net.ApiService
 
-open abstract class BaseRepository {
+ abstract class BaseRepository {
     fun unSubscribe() {
 
     }
-
-    protected lateinit var apiService: ApiService;
-
-    init {
-        if (apiService == null) {
-            apiService = ApiService()
-        }
-    }
+    protected val apiService: ApiService by lazy { ApiService() }
 
 
 }
--- app/src/main/java/com/example/ssl/t_mvvmtest/fragment/ArticleFragment.kt
+++ app/src/main/java/com/example/ssl/t_mvvmtest/fragment/ArticleFragment.kt
@@ -19,6 +19,7 @@ class ArticleFragment : AbsLifecycleFragment<ArticlesViewModel>() {
         super.initView()
         tvName.setOnClickListener {
             getRemoteData()
+            mViewModel!!.mRepository!!.upload()
         }
     }
 
